2a82927ab6fd376ac8c570adc203cdb9867d358c
juliancoronado <juliancoronado@csu.fullerton.edu>
2020-03-11 22:03:26 -0700

    Fixed matrix creation:

--- railfence.py
+++ railfence.py
@@ -2,6 +2,7 @@
 # 03/10/20
 
 import math
+import numpy as np
 
 # gets usr input
 usr_input = input("Enter a string: ")
@@ -10,25 +11,22 @@ plaintext = usr_input.replace(" ", "").lower()
 # gets usr key size input
 key = int(input("Enter a key size: "))
 
-print("Plaintext:", plaintext)
-print("Plaintext Length:", len(plaintext))
-print("Key Size:", key, "\n")
+# print("Plaintext:", plaintext)
+# print("Plaintext Length:", len(plaintext))
+# print("Key Size:", key, "\n")
 
 n = len(plaintext)
-# this is what is wrong (i'm pretty sure)
-m = (n % key) + 1
-# print to display m
-# print("m:", m, "\n")
 
-# generate matrix (m here gives the errors sometimes)
-rail = [["-"] * m] * key
+m = math.floor(n / key)
 
-for i in range(0, m):
-    for j in range(0, key):
-        # test adding zeros to the matrix
-        # needs to fit all characters, vertically then horizontally
-        # running into problems with the matrix size
-        rail[i][j] = "0"
+if n % key > 0:
+    m += 1
 
-for e in rail:
-    print(e)
+# generate matrix using numpy
+railfenceMatrix = np.chararray((key, m))
+
+# temp - assigns all indices to "-"
+railfenceMatrix[:] = '-'
+
+# temp - displays matrix
+print(railfenceMatrix)
